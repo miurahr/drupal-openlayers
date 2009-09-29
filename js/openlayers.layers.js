e4bcf43e699b6e911cb92730a4e8ed3f55d04841
Tom MacWright <tmcw@12664.no-reply.drupal.org>
2009-09-29 16:34:26 +0000

    Changing the layer def so that it doesn't overwrite custom layers

--- js/openlayers.layers.js
+++ js/openlayers.layers.js
@@ -9,21 +9,17 @@
  * @ingroup openlayers
  */
 
-/**
- * Default OpenLayers layer handlers
- * Other handlers are added via ctools plugins
- *
- * */
-Drupal.openlayers.layer = {
-  /*
-   * Creates an OpenLayers.Layer.WMS object
-   * http://dev.openlayers.org/releases/OpenLayers-2.8/doc/apidocs/files/OpenLayers/Layer/WMS-js.html
-   * @param name: name (a key in the map['layers'] array)
-   * @param map: the map object
-   * @param options: a hash of options, currently with object values like bounds as arrays
-   * @return layer object
-   */
-  'WMS': function (name, map, options) {
+Drupal.openlayers.layer = Drupal.openlayers.layer || {};
+
+/*
+ * Creates an OpenLayers.Layer.WMS object
+ * http://dev.openlayers.org/releases/OpenLayers-2.8/doc/apidocs/files/OpenLayers/Layer/WMS-js.html
+ * @param name: name (a key in the map['layers'] array)
+ * @param map: the map object
+ * @param options: a hash of options, currently with object values like bounds as arrays
+ * @return layer object
+ */
+Drupal.openlayers.layer.WMS = function (name, map, options) {
     // TODO: do not permit empty format or type parameters
     if (options.params.format === undefined) {
       options.params.format = "image/png";
@@ -31,9 +27,10 @@ Drupal.openlayers.layer = {
     // TODO: move Bounds and other instantiations into func
     options.options.maxExtent = new OpenLayers.Bounds.fromArray(options.options.maxExtent);
     return new OpenLayers.Layer.WMS(name, options.url, options.params, options.options);
-  },
-  'TMS': function (name, map, options) {
-    var styleMap = Drupal.openlayers.getStyleMap(map, options.name);
+};
+  
+Drupal.openlayers.layer.TMS = function (name, map, options) {
+  var styleMap = Drupal.openlayers.getStyleMap(map, options.name);
     if (options.options.maxExtent !== undefined) {
       options.options.maxExtent = new OpenLayers.Bounds.fromArray(options.options.maxExtent);
     }
@@ -43,9 +40,10 @@ Drupal.openlayers.layer = {
     var layer = new OpenLayers.Layer.TMS(name, options.url, options.options);
     layer.styleMap = styleMap;
     return layer;
-  },
-  'OSM': function (name, map, options) {
-    if (typeof(options.options.maxExtent) !== 'undefined') {
+}
+
+Drupal.openlayers.layer.OSM = function (name, map, options) {
+if (typeof(options.options.maxExtent) !== 'undefined') {
       options.options.maxExtent = new OpenLayers.Bounds.fromArray(options.options.maxExtent);
     }
     if (options.options.type === undefined){
@@ -54,8 +52,9 @@ Drupal.openlayers.layer = {
     var layer = new OpenLayers.Layer.OSM(name, options.url, options.options);  
     layer.attribution = "(c) OpenStreetMap (and) contributors, CC-BY-SA";
     return layer;
-  },
-  'Vector': function (name, map, options) {
+}
+
+Drupal.openlayers.layer.Vector = function (name, map, options) {
     var styleMap = Drupal.openlayers.getStyleMap(map, options.name);
     var layer = new OpenLayers.Layer.Vector(name, {'styleMap': styleMap});
     if (options.features) {
@@ -63,5 +62,4 @@ Drupal.openlayers.layer = {
     }
     layer.title = options.title;
     return layer;
-  }
 };

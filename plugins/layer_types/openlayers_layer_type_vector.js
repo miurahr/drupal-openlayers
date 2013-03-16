c7f9983871e5140a31f0612615504ba945bca7e9
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2013-03-16 10:39:35 +0100

    Replace KML layer type with Vector layer type. This can be extented to use many more formats.

--- plugins/layer_types/openlayers_layer_type_vector.js
+++ plugins/layer_types/openlayers_layer_type_vector.js
@@ -1,24 +1,21 @@
 /**
  * @file
- * Layer handler for KML layers
+ * Layer handler for Vector layers
  */
 
 /**
  * Openlayer layer handler for KML layer
  */
-Drupal.openlayers.layer.kml = function(title, map, options) {
+Drupal.openlayers.layer.vector = function(title, map, options) {
 
   var layer = new OpenLayers.Layer.Vector(title, {
-    name: options.name,
     drupalID: options.drupalID,
     layer_handler: options.layer_handler,
     styleMap: Drupal.openlayers.getStyleMap(map, options.drupalID)
   });
 
-  // KML Projection handling and formating options
-  var kml_options = options.formatOptions;
-  kml_options.internalProjection = new OpenLayers.Projection(map.projection);
-  kml_options.externalProjection = new OpenLayers.Projection(options.projection);
+  options.formatOptions.internalProjection = new OpenLayers.Projection(map.projection);
+  options.formatOptions.externalProjection = new OpenLayers.Projection(options.projection);
 
   if (options.method == 'file' || options.method == 'url') {
     var uri = options.url;
@@ -26,18 +23,25 @@ Drupal.openlayers.layer.kml = function(title, map, options) {
     OpenLayers.Request.GET({
       url: uri,
       callback: function (response) {
-        addFeatures(response.responseText, kml_options);
+        addFeatures(response.responseText, options);
       }
     });
   }
 
   if (options.method == 'raw') {
-    addFeatures(options.raw, kml_options);
+    addFeatures(options.raw, options);
   }
 
-  function addFeatures(kml, options) {
-    var format = new OpenLayers.Format.KML(options);
-    var features = format.read(kml);
+  function addFeatures(vector, options) {
+    switch(options.format) {
+      case 'GPX':
+        var format = new OpenLayers.Format.GPX(options.formatOptions);
+        break;
+      case 'KML':
+        var format = new OpenLayers.Format.KML(options.formatOptions);
+        break;
+    }
+    var features = format.read(vector);
     // Add features, if needed
     if (features) {
       layer.addFeatures(features);

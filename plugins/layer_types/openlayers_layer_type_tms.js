ce93920b7420d6d35bbba69b3a4621dfba88a998
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2013-03-22 10:08:33 +0100

    Merge MapTiler and TMS Layers Types.

--- plugins/layer_types/openlayers_layer_type_tms.js
+++ plugins/layer_types/openlayers_layer_type_tms.js
@@ -8,12 +8,20 @@
  * Openlayer layer handler for TMS layer
  */
 Drupal.openlayers.layer.tms = function(title, map, options) {
-  var styleMap = Drupal.openlayers.getStyleMap(map, options.drupalID);
   if (options.maxExtent !== undefined) {
     options.maxExtent = new OpenLayers.Bounds.fromArray(options.maxExtent);
   }
+
+  if (options.maptiler == true) {
+    options.getURL = function(bounds) {
+      var res = this.map.getResolution();
+      var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
+      var y = Math.round((bounds.bottom - this.tileOrigin.lat) / (res * this.tileSize.h));
+      var z = this.map.getZoom();
+      return this.url + z + '/' + x + '/' + y + '.' + this.type;
+    }
+  }
+
   options.projection = new OpenLayers.Projection(options.projection);
-  var layer = new OpenLayers.Layer.TMS(title, options.url, options);
-  layer.styleMap = styleMap;
-  return layer;
+  return new OpenLayers.Layer.TMS(title, options.url, options);
 };

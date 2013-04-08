11c7c0efd522abb2ee03558eb3331fc5fc3a88b9
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2013-04-08 16:10:03 +0200

    Fix maxExtent parameters in WMS Layer type.

--- plugins/layer_types/openlayers_layer_type_wms.js
+++ plugins/layer_types/openlayers_layer_type_wms.js
@@ -16,23 +16,29 @@ Drupal.openlayers.layer.wms = function(title, map, options) {
     options.params.ratio = parseFloat(options.params.ratio);
   }
 
-  options.params.drupalID = options.drupalID;
+  if (OpenLayers.Util.isArray(options.options.maxExtent)) {
+    options.options.maxExtent = OpenLayers.Bounds.fromArray(options.options.maxExtent);
+  } else {
+    options.options.maxExtent = OpenLayers.Bounds.fromArray(map.maxExtent);
+  }
+
+  options.options.drupalID = options.drupalID;
 
   // Set isBaseLayer explicitly so that OpenLayers does not guess from transparency
-  options.params.isBaseLayer = Boolean(options.isBaseLayer);
+  options.options.isBaseLayer = Boolean(options.isBaseLayer);
 
   // Convert to representation that match with WMS specification
-  var optionsClone = jQuery.extend(true, {}, options.options);
-  if(optionsClone.hasOwnProperty("TRANSPARENT") && optionsClone.TRANSPARENT===0){
-    optionsClone.TRANSPARENT = "FALSE";
-  }
-  if(optionsClone.hasOwnProperty("TRANSPARENT") && optionsClone.TRANSPARENT===1){
-    optionsClone.TRANSPARENT = "FALSE";
-  }
-
   var paramsClone = jQuery.extend(true, {}, options.params);
-  // OpenLayers can calculate the resolutions usually if provided with the number of zoom levels and tile sizes
-  paramsClone.numZoomLevels=18;
+  if(paramsClone.hasOwnProperty("TRANSPARENT") && paramsClone.TRANSPARENT===0){
+    paramsClone.TRANSPARENT = "FALSE";
+  }
+  if(paramsClone.hasOwnProperty("TRANSPARENT") && paramsClone.TRANSPARENT===1){
+    paramsClone.TRANSPARENT = "FALSE";
+  }
 
-  return new OpenLayers.Layer.WMS(title, options.base_url, optionsClone, paramsClone);
+  var optionsClone = jQuery.extend(true, {}, options.options);
+  // OpenLayers can calculate the resolutions usually if provided with the number of zoom levels and tile sizes
+  optionsClone.numZoomLevels=18;
+
+  return new OpenLayers.Layer.WMS(title, options.base_url, paramsClone, optionsClone);
 };

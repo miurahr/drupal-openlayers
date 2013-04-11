34c9aedbd9a5ae79efaf3a0d2e3595a998f9d8e0
Pol Dell'Aiera <Pol@47194.no-reply.drupal.org>
2013-04-11 15:48:55 +0200

    Improvement in the formula to find the azimuth.
    Automatically switch to North or South, according to the position of the compass.

--- plugins/behaviors/openlayers_behavior_compass.js
+++ plugins/behaviors/openlayers_behavior_compass.js
@@ -15,26 +15,30 @@ Drupal.openlayers.addBehavior('openlayers_behavior_compass', function (data, opt
       "default": options.style
     })
   });
-
   layer.events.register('moveend', layer, function(event) {
     layer.removeAllFeatures();
     // TODO: Find a way to calculate the pixel automatically.
     var coords1 = map.getLonLatFromPixel(new OpenLayers.Pixel(map.size.w-90, 90));
-    var coords2 = new OpenLayers.LonLat(0, 90.0).transform(
-      new OpenLayers.Projection('EPSG:4326'),
-      map.getProjectionObject()
-    );
-    var azimuth = 0;
-    // TODO: Is this really necessary ?
-    if (map.zoom > 0) {
-      var x = coords2.lon - coords1.lon;
-      var y = coords2.lat - coords1.lat;
-      var rad = Math.acos( y / Math.sqrt(x*x+y*y) );
-      var factor = x > 0 ? 1:-1;
-      azimuth = Math.round(factor*rad*180/Math.PI);
+    var epsg4326 = new OpenLayers.Projection('EPSG:4326');
+
+    coords1.transform(map.getProjectionObject(), epsg4326);
+    if (parseFloat(coords1.lat) < 0) {
+      var coords2 = new OpenLayers.LonLat(0, -90.0).transform(epsg4326, map.getProjectionObject());
+      var reverse = 180;
+    } else {
+      var coords2 = new OpenLayers.LonLat(0, 90.0).transform(epsg4326, map.getProjectionObject());
+      var reverse = 0;
     }
+    coords1.transform(epsg4326, map.getProjectionObject());
+
+    var x = coords2.lon - coords1.lon;
+    var y = coords2.lat - coords1.lat;
+    var rad = Math.acos( y / Math.sqrt(x*x+y*y) );
+    var factor = x > 0 ? 1:-1;
+    var azimuth = Math.round(factor*rad*180/Math.PI) + reverse;
+
     var feature = new OpenLayers.Feature.Vector(
-      new OpenLayers.Geometry.Point(coords.lon, coords.lat), {angle: azimuth}
+      new OpenLayers.Geometry.Point(coords1.lon, coords1.lat), {angle: azimuth}
     );
     layer.addFeatures(feature);
   });
